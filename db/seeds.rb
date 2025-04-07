0ba891fde6f108eff45dd496bb54a8e3dbf07c1c
Temmarie <temmar_whyte16@yahoo.com>
2025-04-08 00:57:38 +0100

    Set up seed file to reflect models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,3 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
-
-
 employees = [
   { name: "John Doe", department: "Sales" },
   { name: "Jane Doe", department: "Marketing" },
@@ -17,14 +6,15 @@ employees = [
   { name: "Mike Brown", department: "Finance" },
 ]
 
+task_names = ["Setup email", "Create Slack account", "Sign NDA", "Meet team", "Review company policies"]
+
 employees.each do |employee|
   Employee.create!(employee)
 end
 
 Employee.all.each do |employee|
-  3.times do
-    OnboardingTask.create!(employee: employee, task_name: "Task #{rand(1..100)}")
+  task_names.sample(rand(3..5)).each do |task|
+    employee.onboarding_tasks.create!(task_name: task, status: ["pending", "completed"].sample)
   end
 end
 
-  
