928f3724dfcd7be50dabbdc837e7bc6a7b981536
Temmarie <temmar_whyte16@yahoo.com>
2025-04-08 17:16:46 +0100

    Fix enum issues and regulate authoriation

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,12 +1,6 @@
 class Employee < ApplicationRecord
     has_many :onboarding_tasks, dependent: :destroy
-    # belongs_to :department
-    
-    # validates :name, presence: true
-    # validates :email, presence: true, uniqueness: true
-    # validates :department_id, presence: true
-    
-    # def onboarding_status
-    #     onboarding_tasks.where(status: 'incomplete').count == 0 ? 'complete' : 'incomplete'
-    # end
+     belongs_to :user
+
+     validates :name, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,33 @@
 class User < ApplicationRecord
+  has_one :employee, dependent: :destroy
+
   has_secure_password
+
+  ROLES = %w[employee admin].freeze
+
   has_many :sessions, dependent: :destroy
 
-  normalizes :email_address, with: ->(e) { e.strip.downcase }
+  validates :email_address, presence: true, uniqueness: true
+  validates :role, presence: true, inclusion: { in: ROLES }
+
+  before_validation :normalize_email
+
+  def normalize_email
+    self.email_address = email_address.strip.downcase if email_address.present?
+  end
+
+  def admin?
+    role == "admin"
+  end
+
+  def employee?
+    role == "employee"
+  end
+
+  before_validation :set_default_role, on: :create
+
+def set_default_role
+  self.role ||= "employee"
+end
+
 end
