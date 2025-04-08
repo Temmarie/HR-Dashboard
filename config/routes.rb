8de3e131ff2bd7c0a74f629f72df0ec77b1723c0
Temmarie <temmar_whyte16@yahoo.com>
2025-04-08 10:40:34 +0100

    Set up routes to reflect new dashboard home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
-  root "dashboard#index"
-
-  resources :onboarding_tasks, only: [:update, :index] 
-  resources :employees, only: [:index] 
+  root "dashboard#home"
+  resources :onboarding_tasks, only: [:update, :index]
+  resources :employees, only: [:index]
+  get "dashboard" => "dashboard#index", as: :dashboard
   get "up" => "rails/health#show", as: :rails_health_check
-
- 
 end
