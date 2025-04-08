9d1b73e6d8106ce68c13fccc2173ea5934d79498
Temmarie <temmar_whyte16@yahoo.com>
2025-04-08 12:53:11 +0100

    Set up login configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+
   resource :session
   resources :passwords, param: :token
-  root "dashboard#home"
+  resource :registrations, only: [:create, :new]
+  
   resources :onboarding_tasks, only: [:update, :index]
+
   resources :employees, only: [:index]
   get "dashboard" => "dashboard#index", as: :dashboard
+
   get "up" => "rails/health#show", as: :rails_health_check
+
+  root "dashboard#home"
 end
